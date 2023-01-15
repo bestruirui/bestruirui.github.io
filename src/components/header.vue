35d9f1a9c3beefa035260d471afb5bcbb71135ef
bestruirui <1004279909@qq.com>
2023-01-15 05:15:11 +0000

    init

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,23 +1,17 @@
 <template>
-  <div class="ha-header-perspective">
-    <div class="ha-header-front">
-      <h1><span>大标题</span></h1>
-      <nav>
-        <a href="http://tympanus.net/Development/AnimatedBooks/">小标题1</a>
-        <a>小标题2</a>
-        <a>小标题3</a>
-        <a href="http://tympanus.net/codrops/?p=15935">小标题4</a>
-      </nav>
-    </div>
-    <div class="ha-header-bottom">
-      <nav>
-        <a>子标题1</a>
-        <a>子标题2</a>
-        <a>子标题3</a>
-        <a>子标题4</a>
-        <a>子标题5</a>
-        <a>子标题6</a>
-      </nav>
-    </div>
-  </div>
+  <header>
+    <navbar class="rui-header">
+      <div class="rui-header-logo">
+        <a>
+          <span>BESTRUI</span>
+        </a>
+      </div>
+      <navlinks class="hidden dp:flex">
+        <links class="px-2rem">标题</links>
+        <links class="px-2rem">标题</links>
+        <links class="px-2rem">标题</links>
+        <links class="px-2rem">标题</links>
+      </navlinks>
+    </navbar>
+  </header>
 </template>
